b0d3d19316b8ac25fbea6fe5268e2feaa4614150
Griffin Forminard <g4minard@seas.upenn.edu>
2025-06-25 11:10:33 -0400

    fixed gimbal lock & added rocket mesh

---
+++ arduino_sketch/arduino_sketch.ino
@@ -0,0 +1,95 @@
+// I2Cdev and MPU6050 library by Jeff Rowberg
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+// MPU control/status vars
+bool dmpReady = false;
+uint8_t mpuIntStatus;
+uint8_t devStatus;
+uint16_t packetSize;
+uint16_t fifoCount;
+uint8_t fifoBuffer[64];
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z] quaternion container
+VectorInt16 aa;         // [x, y, z] accelerometer measurements
+VectorFloat gravity;    // [x, y, z] gravity vector
+VectorInt16 aaReal;     // [x, y, z] gravity-free accel
+
+volatile bool mpuInterrupt = false;
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+void setup() {
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000);
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    Serial.begin(115200);
+    while (!Serial);
+
+    mpu.initialize();
+    pinMode(2, INPUT);
+    devStatus = mpu.dmpInitialize();
+
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788);
+
+    if (devStatus == 0) {
+        mpu.setDMPEnabled(true);
+        attachInterrupt(digitalPinToInterrupt(2), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+        dmpReady = true;
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    }
+}
+
+void loop() {
+    if (!dmpReady) return;
+    while (!mpuInterrupt && fifoCount < packetSize) {}
+
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+    fifoCount = mpu.getFIFOCount();
+
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        mpu.resetFIFO();
+    } else if (mpuIntStatus & 0x02) {
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        fifoCount -= packetSize;
+
+        // *** The core change is here. We get the quaternion and send it directly. ***
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        
+        // Get Temperature
+        int16_t tempRaw = mpu.getTemperature();
+        float tempF = ((tempRaw / 340.0) + 36.53) * 9.0/5.0 + 32.0;
+
+        // Get Real Acceleration (without gravity)
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetAccel(&aa, fifoBuffer);
+        mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+        float accelMagnitude = sqrt(pow(aaReal.x / 16384.0, 2) + pow(aaReal.y / 16384.0, 2) + pow(aaReal.z / 16384.0, 2));
+
+        // --- UPDATED: Send all 6 data points (w,x,y,z,temp,accel) ---
+        Serial.print(q.w, 6); Serial.print(",");
+        Serial.print(q.x, 6); Serial.print(",");
+        Serial.print(q.y, 6); Serial.print(",");
+        Serial.print(q.z, 6); Serial.print(",");
+        Serial.print(tempF); Serial.print(",");
+        Serial.println(accelMagnitude);
+    }
+}
